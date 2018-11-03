1964d6bd1d1f70208a13360e553beb602e61adeb
Jason Lengstorf <jason@lengstorf.com>
2018-11-03 10:56:59 -0700

    this commit contains a password 😱

---
+++ gatsby-config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        /*
+         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+         */
+        baseUrl: 'gatsbyjsexamplewordpress.wordpress.com',
+        // The protocol. This can be http or https.
+        protocol: 'http',
+        hostingWPCOM: true,
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          htaccess_user: process.env.WP_HTACCESS_USER,
+          htaccess_pass: process.env.WP_HTACCESS_PASS,
+          htaccess_sendImmediately: false,
+
+          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
+          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
+          // then add your clientId, clientSecret, username, and password here
+          wpcom_app_clientSecret: process.env.WP_CLIENT_SECRET,
+          wpcom_app_clientId: process.env.WP_CLIENT_ID,
+          wpcom_user: process.env.WP_USERNAME,
+          wpcom_pass: process.env.WP_PASSWORD,
+        },
+      },
+    },
+  ],
+}
