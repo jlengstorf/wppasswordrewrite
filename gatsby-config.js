a2cd50a36e5c900aa749875c4e53fa078cfcf6ea
Jason Lengstorf <jason@lengstorf.com>
2018-11-03 10:57:27 -0700

    this commit affects the file with a password committed

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: 'gatsbyjsexamplewordpress.wordpress.com',
+        baseUrl: 'mywpsite.wordpress.com',
         // The protocol. This can be http or https.
         protocol: 'http',
         hostingWPCOM: true,
